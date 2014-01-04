c98d6e5cc671ac976169d91af7773dad722ffff1
Veysi Ertekin <ertekin.veysi@gmail.com>
2014-01-03 21:22:36 -0430

    ok

--- src/com/hw02/Main.java
+++ src/com/hw02/Main.java
@@ -8,39 +8,49 @@ import java.security.SecureRandom;
 
 import org.junit.runner.JUnitCore;
 
+//  -Xms512m -Xmx1512m -XX:PermSize=512m -XX:MaxPermSize=128m
 public class Main {
 	private static SecureRandom random = new SecureRandom();
-	public static final char[] ALPHABET = "abcçdefgðhýijklmnoöpqrsþtuüvwyz".toCharArray();
 
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-		PrintWriter print = new PrintWriter(System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "x.txt", "UTF-8");
-
-		int textSize = 1000000;
+		int textSize = 10000000;
 		int patternSize = 3;
 
-		final int MAX_PATTERN_SIZE = 10;
+		final int MAX_PATTERN_SIZE = 15;
 
-		String line = "";
-
+		StringBuilder builder = new StringBuilder();
 		for (; patternSize <= MAX_PATTERN_SIZE; patternSize++) {
 			TestCase.text = generateArray(textSize);
 			TestCase.pattern = generateArray(patternSize);
 
 			JUnitCore.runClasses(TestCase.class);
 
-			print.print(line + textSize + "\t" + patternSize + "\t" + String.copyValueOf(TestCase.pattern) + "\t" + TestCase.map.get("BruteForce_INDEX").intValue() + "\t"
-					+ TestCase.map.get("BruteForce") + "\t" + TestCase.map.get("Horspool_INDEX").intValue() + "\t" + TestCase.map.get("Horspool"));
-			line = "\n";
-
+			builder.append(textSize);
+			builder.append('\t');
+			builder.append(patternSize);
+			builder.append('\t');
+			builder.append(TestCase.map.get("BruteForce_INDEX").intValue());
+			builder.append('\t');
+			builder.append(TestCase.map.get("BruteForce"));
+			builder.append('\t');
+			builder.append(TestCase.map.get("Horspool_INDEX").intValue());
+			builder.append('\t');
+			builder.append(TestCase.map.get("Horspool"));
+			builder.append("\n");
+			
 			TestCase.map.clear();
+			System.gc();
 		}
+
+		PrintWriter print = new PrintWriter(System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "x.txt", "UTF-8");
+		print.print(builder);
 		print.close();
 	}
 
 	public static char[] generateArray(int size) {
 		char[] array = new char[size];
 		for (int i = size - 1, j = 0; i >= 0; i--, j++) {
-			array[j] = ALPHABET[random.nextInt(ALPHABET.length)];
+			array[j] = AAlgorithm.ALPHABET[random.nextInt(AAlgorithm.ALPHABET.length)];
 		}
 		return array;
 	}
--- src/com/hw02/algorithm/Horspool.java
+++ src/com/hw02/algorithm/Horspool.java
@@ -3,10 +3,60 @@ package com.hw02.algorithm;
 import com.hw02.AAlgorithm;
 
 public class Horspool extends AAlgorithm {
+	private static int[] shiftTable = new int[ALPHABET.length];
 
 	@Override
 	public int sort(char[] text, char[] pattern) {
+		prepareTable(pattern);
 
-		return 0;
+		// start with lenght of pattern
+		int i = pattern.length - 1, k;
+		isMatch: while (i < text.length) {
+			k = i;
+			for (int j = pattern.length - 1; j >= 0; j--) {
+				if (pattern[j] == text[k]) {
+					k--;
+					continue;
+				}
+				else {
+					i += shiftTable[getCharIndex(text[i])];
+					continue isMatch;
+				}
+			}
+			return k + 1;
+		}
+
+		return text.length;
+	}
+
+	public int getCharIndex(char i) {
+		int j = 0;
+		for (char ch : ALPHABET) {
+			if (ch == i)
+				return j;
+			j++;
+		}
+		return -1;
+	}
+
+	public void prepareTable(char[] pattern) {
+		for (int i = 0; i < shiftTable.length; i++) {
+			shiftTable[i] = pattern.length;
+		}
+		for (char c : pattern) {
+			shiftTable[getCharIndex(c)] = shiftCount(c, pattern);
+		}
+	}
+
+	// if 'c' exist (but not last char) in pattern get index of right most character,
+	// unless assign lenght of pattern to all characters
+	private Integer shiftCount(char c, char[] pattern) {
+		for (int i = pattern.length - 2/* ignore last char of pattern */; i >= 0; i--) {
+			// rightMostIndex
+			if (pattern[i] == c)
+				return pattern.length - 1 - i; // m-1-n
+		}
+		// if doesn't match of any character in pattern return lenght of pattern
+		return pattern.length;
 	}
 }
